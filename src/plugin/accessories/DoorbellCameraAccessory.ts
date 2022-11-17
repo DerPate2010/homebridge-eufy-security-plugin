781000bca54085122de7cb45b5ceb7ad8e0179bc
Steve Nessen <steve.nessen@outlook.com>
2022-11-17 08:46:48 +0100

    add http doorbell service to camera

--- src/plugin/accessories/DoorbellCameraAccessory.ts
+++ src/plugin/accessories/DoorbellCameraAccessory.ts
@@ -1,5 +1,5 @@
 import { Service, PlatformAccessory } from 'homebridge';
-
+import http from 'http';
 import { EufySecurityPlatform } from '../platform';
 
 // import { HttpService, LocalLookupService, DeviceClientService, CommandType } from 'eufy-node-client';
@@ -53,7 +53,47 @@ export class DoorbellCameraAccessory extends CameraAccessory {
     );
 
     this.doorbellService.setPrimaryService(true);
+    this.initHttpEndpoint();
 
+  }
+  initHttpEndpoint() {
+      const server = http.createServer();
+      server.listen(8081);
+      server.on('request', (request: http.IncomingMessage, response: http.ServerResponse) => {
+        let results = {
+          error: true,
+          message: 'Malformed URL.'
+        };
+        if (request.url) {
+          const spliturl = request.url.split('?');
+          if (spliturl.length == 2) {
+            const name = decodeURIComponent(spliturl[1]).split('=')[0];
+            results = this.httpHandler(spliturl[0], name);
+          }
+        }
+        response.writeHead(results.error ? 500 : 200);
+        response.write(JSON.stringify(results));
+        response.end();
+      });
+  }
+  private httpHandler(fullpath: string, name: string) {
+      const path = fullpath.split('/').filter((value) => value.length > 0);
+      switch (path[0]) {
+        case 'doorbell':
+          this.doorbellService
+            .getCharacteristic(this.platform.Characteristic.ProgrammableSwitchEvent)
+            .updateValue(this.platform.Characteristic.ProgrammableSwitchEvent.SINGLE_PRESS);
+          return {
+            error: false,
+            message: 'Doorbell switched on.'
+          };;
+          break;
+        default:
+          return {
+            error: true,
+            message: 'First directory level must be "motion" or "doorbell", got "' + path[0] + '".'
+          };
+      }
   }
 
   // We receive 2 push when Doorbell ring, mute the second by checking if we already send
